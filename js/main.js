4b3e8945b3faaa04412dc6130f22a740444b0207
Bekzod97-div <xushmatofbekzod@gmail.com>
2023-02-12 11:16:46 +0500

    add url copy btn click handler. Wrap form in if statment

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 const elLinkForm = document.querySelector('.js__link-form');
 const elLinkResult = document.querySelector('.link-result');
+const elFormContainer = document.querySelector('.form__container');
 
-elLinkForm.addEventListener('submit', function(evt) {
-  evt.preventDefault();
+if(elLinkForm) {
+  elLinkForm.addEventListener('submit', function(evt) {
+    evt.preventDefault();
 
-  elLinkResult.classList.add('link-result--open');
-})
+    elLinkResult.classList.add('link-result--open');
+  });
+}
+
+if(elFormContainer) {
+  elFormContainer.addEventListener('click', function(evt) {
+    if (evt.target.matches('.js-short-link__copy-btn')) {
+      // Change button text
+      evt.target.textContent = 'Copied!';
+
+      // Change button
+      evt.target.classList.add('short-link__copy-btn--copied');
+
+      // Reset button text and class after 1 second
+      setTimeout(function () {
+        evt.target.textContent = 'Copy';
+        evt.target.classList.remove('short-link__copy-btn--copied');
+      }, 1000);
+    };
+  });
+}
